ed785e23b0904d3921898adc7c25389dd7bdd52b
Christiaan Swart <christiaan.swart@epiuse.com>
2024-11-19 07:22:12 +0200

    Correct the syntax error and ensure the IonRefresher component is properly integrated.

--- frontend/src/views/TabbedView.vue
+++ frontend/src/views/TabbedView.vue
@@ -1,35 +1,33 @@
 <template>
-	<ion-page>
-		<ion-tabs>
-			<ion-router-outlet></ion-router-outlet>
-			<BottomTabs />
-		</ion-tabs>
-		</ion-header>
-		<ion-content>
-			<ion-refresher slot="fixed" @ionRefresh="doRefresh">
-				<ion-refresher-content></ion-refresher-content>
-			</ion-refresher>
-	</ion-page>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <BottomTabs />
+    </ion-tabs>
+    <ion-header>
+    </ion-header>
+    <ion-content>
+      <ion-refresher slot="fixed" @ionRefresh="doRefresh">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup>
 import { IonTabs, IonPage, IonRouterOutlet } from "@ionic/vue"
 import BottomTabs from "@/components/BottomTabs.vue"
 </script>
+
 <script>
 import { ref } from "vue"
 import { IonRefresher, IonRefresherContent } from "@ionic/vue"
+import { employeeResource, userResource } from "@/data"
 
 export default {
   components: {
     IonRefresher,
     IonRefresherContent,
-  },
-  beforeRouteEnter(to, from, next) {
-    refreshData(to, next)
-  },
-  beforeRouteUpdate(to, from, next) {
-    refreshData(to, next)
   },
   setup() {
     const doRefresh = async (event) => {
@@ -45,26 +43,5 @@ export default {
       doRefresh,
     }
   }
-}
-
-function refreshData(to, next) {
-  const { name } = to
-  if (name === "Home") {
-    // Reload home-specific resources
-    // Example: await homeResource.reload()
-  } else if (name === "AttendanceDashboard") {
-    // Reload attendance-specific resources
-    // Example: await attendanceResource.reload()
-  } else if (name === "LeavesDashboard") {
-    // Reload leaves-specific resources
-    // Example: await leaveResource.reload()
-  } else if (name === "ExpenseClaimsDashboard") {
-    // Reload expense claims-specific resources
-    // Example: await expenseResource.reload()
-  } else if (name === "SalarySlipsDashboard") {
-    // Reload salary slips-specific resources
-    // Example: await salarySlipResource.reload()
-  }
-  next()
 }
 </script>
