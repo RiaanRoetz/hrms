13b89f57bf31ff7cbf7f7e2d4e8c7ba5bc34ff9b
Christiaan Swart <christiaan.swart@epiuse.com>
2024-11-19 07:34:32 +0200

    Revert to original code.

--- frontend/src/views/TabbedView.vue
+++ frontend/src/views/TabbedView.vue
@@ -1,47 +1,13 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <BottomTabs />
-    </ion-tabs>
-    <ion-header>
-    </ion-header>
-    <ion-content>
-      <ion-refresher slot="fixed" @ionRefresh="doRefresh">
-        <ion-refresher-content></ion-refresher-content>
-      </ion-refresher>
-    </ion-content>
-  </ion-page>
+	<ion-page>
+		<ion-tabs>
+			<ion-router-outlet></ion-router-outlet>
+			<BottomTabs />
+		</ion-tabs>
+	</ion-page>
 </template>
 
 <script setup>
 import { IonTabs, IonPage, IonRouterOutlet } from "@ionic/vue"
 import BottomTabs from "@/components/BottomTabs.vue"
-</script>
-
-<script>
-import { ref } from "vue"
-import { IonRefresher, IonRefresherContent } from "@ionic/vue"
-import { employeeResource, userResource } from "@/data"
-
-export default {
-  components: {
-    IonRefresher,
-    IonRefresherContent,
-  },
-  setup() {
-    const doRefresh = async (event) => {
-      // Reload relevant resources
-      await employeeResource.reload()
-      await userResource.reload()
-      // Add other resources as needed
-
-      event.target.complete()
-    }
-
-    return {
-      doRefresh,
-    }
-  }
-}
 </script>
